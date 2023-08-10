14360179e8fbd81be4efb4ae7d4816916a418fe0
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2023-08-10 17:51:07 +0200

    CRUD Post partie 1

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -21,10 +21,22 @@ module.exports.signUpErrors = (err) => {
 module.exports.signInErrors = (err) => {
   let errors = { email: "", password: "" };
 
-  if (err.message.includes("email")) errors.email = "Email inconnu2";
+  if (err.message.includes("email")) errors.email = "Email inconnu";
 
   if (err.message.includes("password"))
     errors.password = "Le mot de passe ne correspond pas";
+
+  return errors;
+};
+
+module.exports.uploadErrors = (err) => {
+  let errors = { format: "", maxSize: "" };
+
+  if (err.message.includes("invalid file"))
+    errors.format = "Format incompatabile";
+
+  if (err.message.includes("max size"))
+    errors.maxSize = "Le fichier dépasse 500ko";
 
   return errors;
 };
